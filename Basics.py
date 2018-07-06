17b7967a75dd0289867e26552e666f7e2f9366ad
Bonamichi <marco.bom.amigo@gmail.com>
2018-07-06 17:49:40 -0300

    Print

--- Basics.py
+++ Basics.py
@@ -4,3 +4,5 @@ pi = 3.1415
 print(nome + " " + str(30))
 print("Valor de pi = %s" %(pi))
 print("Valor de pi = %1.2f" % (pi))
+
+print("O primeiro valor é %s, o segundo %s e o terceiro %s" %(1, 2, 3))
