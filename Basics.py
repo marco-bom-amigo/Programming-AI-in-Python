b17b64bc754c67d796ac3295831ec45a7c7af3fb
Bonamichi <marco.bom.amigo@gmail.com>
2018-07-06 18:37:13 -0300

    indentation

--- Basics.py
+++ Basics.py
@@ -2,14 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import random
 
-nome = "José da Silva"
+nome = "Marco A. Bonamichi Jr."
 idade = 30
 pi = 3.1415
 print(nome + " " + str(30))
-print("Valor de pi = %s" %(pi))
-print("Valor de pi = %1.2f" % (pi))
+print("Valor de pi = %s" % pi)
+print("Valor de pi = %1.2f" % pi)
 
-print("O primeiro valor é %s, o segundo %s e o terceiro %s" %(1, 2, 3))
+print("O primeiro valor é %s, o segundo %s e o terceiro %s" % (1, 2, 3))
 
 print("Documento: {a}, Processo: {a}".format(a="processado"))
 print("Documento: {a}, Processo: {b}".format(a="processado", b="erro"))
