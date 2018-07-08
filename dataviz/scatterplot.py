e275b35f753b5d6a8a26210a64a746f3ad298193
Bonamichi <marco.bom.amigo@gmail.com>
2018-07-08 16:49:27 -0300

    Scatterplot

--- dataviz/scatterplot.py
+++ dataviz/scatterplot.py
@@ -9,4 +9,23 @@ plt.style.use('seaborn')
 df_pimas = pd.read_csv("../pandas read/pima-data-norm-city.csv", index_col=0)
 print(df_pimas.head(5))
 
+sns.lmplot("age", "num_preg", df_pimas)
+plt.show()
 
+sns.lmplot("age", "diab_pred", df_pimas)
+plt.show()
+
+sns.lmplot("age", "glucose_conc", df_pimas)
+plt.show()
+
+sns.lmplot("diab_pred", "glucose_conc", df_pimas, order=2)
+plt.show()
+
+sns.lmplot("insulin", "glucose_conc", df_pimas)
+plt.show()
+
+sns.lmplot("insulin", "diab_pred", df_pimas)
+plt.show()
+
+sns.lmplot("bmi", "diab_pred", df_pimas)
+plt.show()
