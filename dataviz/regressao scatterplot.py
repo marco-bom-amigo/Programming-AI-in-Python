3d20764e1d07e10293e426bc4062775e7b95e02a
Bonamichi <marco.bom.amigo@gmail.com>
2018-07-08 16:04:52 -0300

    Data viz 2

--- dataviz/regressao scatterplot.py
+++ dataviz/regressao scatterplot.py
@@ -9,3 +9,11 @@ print(data_frame_1)
 
 sns.lmplot(data=data_frame_1, y="Avaliação",x="Custo")
 plt.show()
+
+data_frame_2 = pd.read_excel("bilheteria.xlsx")
+print(data_frame_2)
+
+fig, ax = plt.subplots()
+fig.set_size_inches(18, 8)
+plt.plot(data_frame_2["Data"], data_frame_2["Bilheteria"])
+plt.show()
