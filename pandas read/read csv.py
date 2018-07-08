d80181a5239fc10e8d6e98e98357526fd9e9d6f1
Bonamichi <marco.bom.amigo@gmail.com>
2018-07-08 13:29:56 -0300

    Pandas read

---
+++ pandas read/read csv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data_frame_csv = pd.read_csv("exemplo.csv")
+print(data_frame_csv)
+
+data_frame_csv = pd.read_csv("exemplo.csv", sep = ";")
+print(data_frame_csv)
+
+data_frame_csv = pd.read_csv("exemplo.csv", sep = ";", header = None)
+print(data_frame_csv)
+
+data_frame_csv = pd.read_csv("exemplo.csv", sep = ";", header = None, nrows = 3)
+print(data_frame_csv)
+
+data_frame_csv = pd.read_csv("exemplo.csv", sep = ";", names = ["Ano", "Livros","Jogos", "Cadernos"], skiprows = 2)
+print(data_frame_csv)
+print(data_frame_csv.describe())
